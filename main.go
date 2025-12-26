a8f739c29c6999dfb139556c79ff5220cdb23e5e
Leo Claudio <11918571+zllec@users.noreply.github.com>
2025-12-26 08:17:21 +0800

    feat: add working logic

--- main.go
+++ main.go
@@ -2,22 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-func checkPrefix(s string) string {
+func checkPrefix(s string) int {
 	negative := strings.HasPrefix(s, "L")
 	if negative {
-		return "NEGATIVE"
+		value, _ := strconv.Atoi(s[1:])
+		return -value
 	}
-	return "POSITIVE"
+	value, _ := strconv.Atoi(s[1:])
+	return value
 }
 
 func main() {
-	// startingPoint := 50
-	input := []string{"L68", "L30", "R48"}
+	dial := 50
+	input := []string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"}
 
 	for _, i := range input {
-		fmt.Println(checkPrefix(i))
+		fmt.Printf("Current dial: %d\n", dial)
+		fmt.Printf("Next input: %s\n", i)
+		result := dial + checkPrefix(i)
+		fmt.Printf("Result: %d\n", result)
+		if result < 0 {
+			dial = result + 100
+		} else if result > 100 {
+			dial = result - 100
+		} else {
+			dial = result
+		}
+		if dial == 100 {
+			dial = 0
+		}
+		fmt.Println(dial)
 	}
 }
